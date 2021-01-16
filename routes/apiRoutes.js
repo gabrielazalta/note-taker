0c65be09c4f5a3dd0d56e09140bd641550e201a0
Gabriela Zalta <gzalta96@gmail.com>
2021-01-16 16:15:03 -0500

    added delete note function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,7 @@ let db = require('../db/db.json');
 
 //localhost:3001/api
 router.get('/notes', (req, res) => {
-    res.json(db);
+    return res.json(db);
 });
 
 router.post('/notes', (req, res) => {
@@ -17,6 +17,24 @@ router.post('/notes', (req, res) => {
     db.push(req.body)
     console.log(db);
 
+    fs.writeFile("db/db.json", JSON.stringify(db), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let deletedNote = req.params.id.toString();
+
+    for (i = 0; i < db.length; i++) {
+        if (db[i].id == deletedNote) {
+            res.send(db[i]);
+
+            db.splice(i, 1);
+            break;
+        }
+    }
     fs.writeFile("db/db.json", JSON.stringify(db), function (err) {
         if (err) {
             console.log(err);
--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// // Use apiRoutes
- app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
